b273f4df2a967323849309a39f6cab1b6e13d150
Antoniolm <codiak19@gmail.com>
2017-04-23 18:39:17 +0200

    A few changes in the structure of our tests

--- Practica2/test/Test.scala
+++ Practica2/test/Test.scala
@@ -2,10 +2,14 @@
   * Created by LENOVO on 18/04/2017.
   */
 
+import org.junit.Test
 import junit.framework.TestCase
 import org.junit.Assert._
 
-object Test extends TestCase{
-  assert(0<1)
-  println("yep")
+class Test extends TestCase{
+
+  def test{
+    assert(2>1)
+    println("yep")
+  }
 }
--- Practica2/test/test.java
+++ Practica2/test/test.java
@@ -2,6 +2,8 @@
  * Created by LENOVO on 22/04/2017.
  */
 
-import org.scalatest;
+//import org.scalatest.junit.*;
 public class test {
+    /*val lista3:List[Int]=List(1,2,4,5,7,45,67,98,123)
+    println("Resultado="+busquedaBinaria(5,lista3,(x:Int,y:Int)=>x<y))*/
 }
