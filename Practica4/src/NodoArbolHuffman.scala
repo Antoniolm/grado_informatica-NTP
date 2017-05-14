554c4d399e5e8ad125094ccf3946c0d2aba9b6e1
Antoniolm <codiak19@gmail.com>
2017-05-14 16:33:30 +0200

    Added toString() methods

--- Practica4/src/NodoArbolHuffman.scala
+++ Practica4/src/NodoArbolHuffman.scala
@@ -7,9 +7,23 @@ abstract class NodoArbolHuffman {
 
 class NodoInternoArbolHuffman(val hijoDerecha:NodoArbolHuffman,val hijoIzquierda:NodoArbolHuffman,
                               val elementos:List[String] , val peso:Int) extends NodoArbolHuffman{
-
+  /**
+    * Crea una cadena con el contenido del Nodo interno
+    * @return
+    */
+  override def toString(): String = {
+    val resultado= "[Nodo Interno] Elementos ->"+ elementos.toString()+", peso -> "+peso
+    resultado
+  }
 }
 
 class NodoHojaArbolHuffman(val elemento:Char , val peso:Int) extends NodoArbolHuffman{
-  
+  /**
+    * Crea una cadena con el contenido del Nodo hoja
+    * @return
+    */
+  override def toString(): String = {
+    val resultado= "[Nodo Hoja] Elemento ->"+ elemento+", peso -> "+peso
+    resultado
+  }
 }
