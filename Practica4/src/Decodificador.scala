cce1cc5a093e83d976cb4c24e3bb833c03a5a6f2
Antoniolm <codiak19@gmail.com>
2017-05-14 23:50:52 +0200

    The decode show the last word now

--- Practica4/src/Decodificador.scala
+++ Practica4/src/Decodificador.scala
@@ -2,30 +2,17 @@
   * Created by LENOVO on 14/05/2017.
   */
 class Decodificador(val raiz:NodoArbolHuffman,val cadena:List[Int]) {
+
+
   def decodificar():String = {
-
-    /*def decodLetra(raiz:NodoArbolHuffman, cadena:List[Int]):Char = {
-
-      if(raiz.tipo=="Hoja")raiz.obtenerCaracteres(0)
-      else {
-        val actual=cadena.head
-        println("Elemento-->"+actual)
-        val raizConv=raiz.asInstanceOf[NodoInternoArbolHuffman]
-
-        if (actual == 1) decodLetra(raizConv.hijoIzquierda,cadena.tail)
-        else decodLetra(raizConv.hijoDerecha,cadena.tail)
-      }
-
-    }
-    decodLetra(raiz,cadena)*/
 
     def auxiliar(nodos:NodoArbolHuffman, cadena:List[Int],resultado:String):String = {
 
-      if(cadena.isEmpty) resultado
+      if(cadena.isEmpty) resultado + nodos.obtenerCaracteres(0)
       else {
         val actual=cadena.head
         if(nodos.tipo=="Hoja") {
-          auxiliar(raiz,cadena,resultado + nodos.obtenerCaracteres(0).toString)
+          auxiliar(raiz,cadena,resultado + nodos.obtenerCaracteres(0))
         }
         else {
           val nodoConv=nodos.asInstanceOf[NodoInternoArbolHuffman]
