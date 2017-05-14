e5b35d54741426e2eb61ca7a2d874bbf499a2895
Antoniolm <codiak19@gmail.com>
2017-05-14 17:59:35 +0200

    A few more changes added

--- Practica4/src/ArbolCodificacion.scala
+++ Practica4/src/ArbolCodificacion.scala
@@ -2,6 +2,7 @@
   * Created by LENOVO on 14/05/2017.
   */
 class ArbolCodificacion(val cadena:String) {
+  val raiz = Nil
 
   def calcularFrecuencia: List[(Char,Int)] ={
     var resultado:List[(Char, Int)] = List()
@@ -12,19 +13,47 @@ class ArbolCodificacion(val cadena:String) {
     for (elemento <- cadena){
         if(lastChar!=elemento) {
           resultado = resultado ::: List((lastChar, cont))
-          println("N- "+cont+ " - "+ lastChar)
           lastChar = elemento
           cont = 0
         }
       cont+=1
     }
 
+    resultado=resultado.sortBy(_._2)
+    println(resultado(0).toString())
     resultado
   }
+
+  //Hacer singleton
+  def unNodo(nodos:List[NodoArbolHuffman]) : Boolean ={
+    if(nodos.length==1)
+      true
+    else
+      false
+  }
+
+  def generarArbol()={
+    val resultado=calcularFrecuencia
+    var nodeList:List[NodoArbolHuffman] = List()
+
+    for(elemento <- resultado){
+      nodeList = nodeList ::: List(NodoHojaArbolHuffman(elemento._1,elemento._2))
+    }
+
+    //while del metodo repetir
+    while(unNodo(nodeList)){
+
+
+    }
+
+  }
+
 
 }
 
 object ArbolCodificacion extends App{
+  def apply(cadena:String ) = new ArbolCodificacion(cadena)
+
   val arbol=new ArbolCodificacion("AAAAAAAABBBCDEFGH")
   arbol.calcularFrecuencia
 }
