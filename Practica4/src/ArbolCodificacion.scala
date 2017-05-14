d86aaa78ace1ff79e8c384eef82871b3417d890b
Antoniolm <codiak19@gmail.com>
2017-05-14 17:21:54 +0200

    The user can know the weight of each character

--- Practica4/src/ArbolCodificacion.scala
+++ Practica4/src/ArbolCodificacion.scala
@@ -1,6 +1,30 @@
 /**
   * Created by LENOVO on 14/05/2017.
   */
-class ArbolCodificacion {
+class ArbolCodificacion(val cadena:String) {
 
+  def calcularFrecuencia: List[(Char,Int)] ={
+    var resultado:List[(Char, Int)] = List()
+
+    var lastChar:Char = cadena(0)
+    var cont:Int = 0
+
+    for (elemento <- cadena){
+        if(lastChar!=elemento) {
+          resultado = resultado ::: List((lastChar, cont))
+          println("N- "+cont+ " - "+ lastChar)
+          lastChar = elemento
+          cont = 0
+        }
+      cont+=1
+    }
+
+    resultado
+  }
+
+}
+
+object ArbolCodificacion extends App{
+  val arbol=new ArbolCodificacion("AAAAAAAABBBCDEFGH")
+  arbol.calcularFrecuencia
 }
